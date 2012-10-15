66d7124317f46e36ccaaed286b4f6e118bf2564e
sharkpp <webmaster@sharkpp.net>
2012-10-15 15:35:46 +0000

    ソースの改行コードをLfに統一。 TODO内容を整理
    git-svn-id: http://nasserver:888/repos/main/platform/Titanium/Tumbdash@2365 fd21481f-4f5f-b64a-a7cc-914020ce4b36

--- Resources/ui/tablet/MainWindow.js
+++ Resources/ui/tablet/MainWindow.js
@@ -37,7 +37,6 @@ module.exports = (function() {
 					color : '#fff',
 					backgroundSelectedColor: '#0000',
 					textAlign: 'left',
-					textAlign: 'left',
 				},
 			'toolbar': {
 					height: '48dp',
